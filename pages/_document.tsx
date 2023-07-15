35ad04c1a4245ca51d267d249a0757af27b29050
Leandro Cardozo <50833557+leanczo@users.noreply.github.com>
2023-07-15 00:49:09 -0300

    ✨ add analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import siteMetadata from '@/data/siteMetadata'
+import { Analytics } from '@vercel/analytics/react'
 
 class MyDocument extends Document {
   render() {
@@ -29,6 +30,7 @@ class MyDocument extends Document {
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
           <NextScript />
+          <Analytics />
         </body>
       </Html>
     )
