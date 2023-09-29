7b892f0aa25d55aeff50149f7ffd8b666cbffff7
Leandro Cardozo <50833557+leanczo@users.noreply.github.com>
2023-09-29 19:22:57 -0300

    ☕ add new entry as draft, fix labels

--- pages/blog/[...slug].tsx
+++ pages/blog/[...slug].tsx
@@ -51,7 +51,7 @@ export default function BlogPostPage({
       {'draft' in post && post.draft === true ? (
         <div className="mt-24 text-center">
           <PageTitle>
-            Under Construction{' '}
+            En construcción{' '}
             <span role="img" aria-label="roadwork sign">
               🚧
             </span>
