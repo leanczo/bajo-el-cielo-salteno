e00e3b91e5b77fe3c765cd0851c4ce3d17aa1780
Leandro Cardozo <50833557+leanczo@users.noreply.github.com>
2023-10-13 21:10:25 -0300

    ☕ add project "el bolson"

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,3 +1,10 @@
-const projectsData = []
+const projectsData = [
+  {
+    title: 'El Bolsón',
+    description: `El Bolsón, en el corazón de la Patagonia argentina, ofrece paisajes majestuosos y refugios de montaña visitables, convirtiéndolo en un oasis para quienes aman la naturaleza y la aventura`,
+    imgSrc: '/static/images/el-bolson/cajon-del-azul.jpeg',
+    href: '/blog/el-bolson',
+  },
+]
 
 export default projectsData
